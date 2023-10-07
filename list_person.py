d4d9323d06400804e043fb7eba9c01d1bf4171b8
SpedicatiDaniele <114939595+SpedicatiDaniele@users.noreply.github.com>
2023-10-07 18:38:31 +0200

    POST CHIAMATA

--- list_person.py
+++ list_person.py
@@ -12,33 +12,13 @@ def list_person (txt):
     wierd_name = {'one-name':[], 'more-than-two-names':[]}
     final_text = ""
     for n in result:
-        if len(n) == 2:
-            if n[0][0].isupper():
-                text = """
-                <person xml:id='{}' sameAs=''>
-                    <persName>
-                        <forename>{}</forename>
-                        <surname>{}</surename>
-                    <persName>
-                <person>\n
-                """.format(n[0][0]+n[1], n[0], n[1])
-                final_text + text
-            else:
-                text = """
-                <person xml:id='{}' sameAs=''>
-                    <persName>
-                        <forename>{}</forename>
-                        <roleName>{}</roleName>
-                    <persName>
-                <person>\n
-                """.format(n[0][0]+n[1], n[1], n[0].capitalize())
-                final_text + text
-        elif len(n) == 1:
-            wierd_name['one-name'].append(n)
-        else:
-            wierd_name['more-than-two-names'].append(n)
+        text = """
+        <person xml:id='{}'>
+            <persName>{}<persName>
+        <person>\n
+        """.format(n[0][0]+n[1], name)
+        final_text + text
             
-    
     with open('final-list.txt', 'w', encoding='utf-8') as f:
         f.write(final_text)
 
